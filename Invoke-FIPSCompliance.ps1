36c596cb1ff8de1668654393bf06f43bc122d65e
smooti <66232763+smooti@users.noreply.github.com>
2020-12-30 20:40:27 -0500

    Making a script instead of just a function
    Converting to script instead of just a function. Removing call of function.

--- Invoke-FIPSCompliance.ps1
+++ Invoke-FIPSCompliance.ps1
@@ -1,50 +1,45 @@
-# Will enforce FIPS to all .exe.config files
+# Will enforce FIPS on all .exe.config files
 
-function Invoke-FIPSCompliance {
-    [CmdletBinding()]
-    param (
-        [ValidateCount(0, 1)]
-        [String[]] $Path = 'C:\',
+param (
+    [ValidateCount(0, 1)]
+    [String[]] $Path = 'C:\',
 
-        [switch] $AllDrives
-    )
+    [switch] $AllDrives
+)
 
-    if ($AllDrives) {
-        $drives = [System.IO.DriveInfo]::getdrives() | Where-Object { $_.DriveType -eq 'Fixed' } | Select-Object -ExpandProperty 'Name'
+if ($AllDrives) {
+    $drives = [System.IO.DriveInfo]::getdrives() | Where-Object { $_.DriveType -eq 'Fixed' } | Select-Object -ExpandProperty 'Name'
 
-        ForEach ($d in $drives) {
-            Write-Host "Scanning drive '$($d)' for non-compliant files...`n" -ForegroundColor Cyan
-            $files = @(Get-ChildItem -Path "$($d)*.exe.config" -Recurse -ErrorAction SilentlyContinue).FullName 
-        
-            ForEach ($f in $files) {
-                Try {
-                    if (-NOT($f -like "*KeePass*")) {
-                        # (Get-Content -path $i -Raw) -replace '<enforceFIPSPolicy enabled="false"/>','<enforceFIPSPolicy enabled="true"/>' | Set-Content -Path $i
-                    }
-                }
-                Catch {
-                    Write-Warning "Unable to enforce FIPS on '$($f)'"
+    ForEach ($d in $drives) {
+        Write-Host "Scanning drive '$($d)' for non-compliant files...`n" -ForegroundColor Cyan
+        $files = @(Get-ChildItem -Path "$($d)*.exe.config" -Recurse -ErrorAction SilentlyContinue).FullName 
+    
+        ForEach ($f in $files) {
+            Try {
+                if (-NOT($f -like "*KeePass*")) {
+                    # (Get-Content -path $i -Raw) -replace '<enforceFIPSPolicy enabled="false"/>','<enforceFIPSPolicy enabled="true"/>' | Set-Content -Path $i
                 }
             }
-        }
-        Write-Host "All drives have been FIPS Enforced!" -ForegroundColor Green
-        Exit
-    }  
-
-    Write-Host "Scanning '$($Path)' for non-compliant files...`n" -ForegroundColor Cyan
-    $files = @(Get-ChildItem -Path "$($Path)*.exe.config" -Recurse -ErrorAction SilentlyContinue).FullName 
-
-    ForEach ($f in $files) {
-        Try {
-            if (-NOT($f -like "*KeePass*")) {
-                # (Get-Content -path $i -Raw) -replace '<enforceFIPSPolicy enabled="false"/>','<enforceFIPSPolicy enabled="true"/>' | Set-Content -Path $i
+            Catch {
+                Write-Warning "Unable to enforce FIPS on '$($f)'"
             }
-        }
-        Catch {
-            Write-Warning "Unable to enforce FIPS on '$($f)'"
         }
     }
-    Write-Host "All files have been FIPS enforced!" -ForegroundColor Green
-}
+    Write-Host "All drives have been FIPS Enforced!" -ForegroundColor Green
+    Exit
+}  
 
-Invoke-FIPSCompliance -Path "F:\Scripts\Powershell"
+Write-Host "Scanning '$($Path)' for non-compliant files...`n" -ForegroundColor Cyan
+$files = @(Get-ChildItem -Path "$($Path)*.exe.config" -Recurse -ErrorAction SilentlyContinue).FullName 
+
+ForEach ($f in $files) {
+    Try {
+        if (-NOT($f -like "*KeePass*")) {
+            # (Get-Content -path $i -Raw) -replace '<enforceFIPSPolicy enabled="false"/>','<enforceFIPSPolicy enabled="true"/>' | Set-Content -Path $i
+        }
+    }
+    Catch {
+        Write-Warning "Unable to enforce FIPS on '$($f)'"
+    }
+}
+Write-Host "All files have been FIPS enforced!" -ForegroundColor Green
